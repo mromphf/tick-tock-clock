6d9d1e6abef1a70380b990a1543909e0867fa9dc
Mark Romphf <markromphf@gmail.com>
2019-04-19 14:03:42 -0600

    Add function for drawing empty circle.

--- src/tick_tock_clock/core.cljs
+++ src/tick_tock_clock/core.cljs
@@ -2,17 +2,27 @@
 
 (def width (.-innerWidth js/window))
 (def height (.-innerHeight js/window))
+(def doublePi (* 2 (.-PI js/Math)))
 
 (def canvas (.getElementById js/document "clock"))
 (def context (.getContext canvas "2d"))
 (set! (.-width canvas) width)
 (set! (.-height canvas) height)
+(set! (.-lineWidth context) 5)
 (set! (.-fillStyle context) "#00f")
+(set! (.-strokeStyle context) "#00f")
 
-(defn drawCircle [& {:keys [x y radius]}]
+(defn drawSolidCircle [& {:keys [x y radius]}]
   (.beginPath context)
-  (.arc context x y radius 0 (* 2 3.14))
+  (.arc context x y radius 0 doublePi)
   (.closePath context)
   (.fill context))
 
-(drawCircle :x (/ width 2) :y (/ height 2) :radius 25)
+(defn drawEmptyCircle [& {:keys [x y radius]}]
+  (.beginPath context)
+  (.arc context x y radius 0 doublePi)
+  (.closePath context)
+  (.stroke context))
+
+(drawSolidCircle :x (/ width 2) :y (/ height 2) :radius 25)
+(drawEmptyCircle :x (/ width 2) :y (/ height 2) :radius 250)
