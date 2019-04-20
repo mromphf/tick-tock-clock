c3767c8cf8fd3c44face03193dff3d16ae163827
Mark Romphf <markromphf@gmail.com>
2019-04-20 15:11:42 -0600

    Clean up.

--- src/tick_tock_clock/core.cljs
+++ src/tick_tock_clock/core.cljs
@@ -9,10 +9,9 @@
 (def context (.getContext canvas "2d"))
 (set! (.-width canvas) width)
 (set! (.-height canvas) height)
-(set! (.-strokeStyle context) "#00f")
-(set! (.-lineWidth context) 5)
 
 (defn drawSolidCircle [& {:keys [x y radius fill]}]
+  (set! (.-lineWidth context) 10)
   (set! (.-fillStyle context) fill)
   (.beginPath context)
   (.arc context x y radius 0 doublePi)
@@ -20,22 +19,27 @@
   (.fill context))
 
 (defn drawEmptyCircle [& {:keys [x y radius fill]}]
-  (set! (.-fillStyle context) fill)
+  (set! (.-strokeStyle context) "#00f")
   (.beginPath context)
   (.arc context x y radius 0 doublePi)
   (.closePath context)
   (.stroke context))
 
 (defn drawHand [pos]
+  (set! (.-lineWidth context) 5)
   (.beginPath context)
   (.moveTo context 0 0)
   (.rotate context pos)
-  (.lineTo context 0 (- 200))
+  (.lineTo context 0 (- (* 250 0.9)))
   (.stroke context)
   (.rotate context (- pos)))
 
 (defn drawClock []
-  (def seconds (.getSeconds (js/Date.)))
+  (def now (js.Date.))
+  (def hours (.getHours now))
+  (def minutes (.getMinutes now))
+  (def seconds (.getSeconds now))
+
   (drawSolidCircle :x 0 :y 0 :radius 250 :fill "#fff")
   (drawSolidCircle :x 0 :y 0 :radius 25 :fill "#00f")
   (drawEmptyCircle :x 0 :y 0 :radius 250 :fill "#00f")
