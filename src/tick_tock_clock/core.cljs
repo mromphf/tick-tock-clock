4848e01ee9bbf45a9189e1d3f959377d06378dfc
Mark Romphf <markromphf@gmail.com>
2019-04-19 18:13:01 -0600

    Render a rotating 'seconds' hand.

--- src/tick_tock_clock/core.cljs
+++ src/tick_tock_clock/core.cljs
@@ -2,35 +2,44 @@
 
 (def width (.-innerWidth js/window))
 (def height (.-innerHeight js/window))
-(def doublePi (* 2 (.-PI js/Math)))
+(def pi (.-PI js/Math))
+(def doublePi (* 2 pi))
 
 (def canvas (.getElementById js/document "clock"))
 (def context (.getContext canvas "2d"))
 (set! (.-width canvas) width)
 (set! (.-height canvas) height)
-(set! (.-lineWidth context) 5)
-(set! (.-fillStyle context) "#00f")
 (set! (.-strokeStyle context) "#00f")
+(set! (.-lineWidth context) 5)
 
-(defn drawBigHand [& {:keys [x y]}]
-  (.beginPath context)
-  (.moveTo context x y)
-  (.lineTo context x (- y 200))
-  (.closePath context)
-  (.stroke context))
-
-(defn drawSolidCircle [& {:keys [x y radius]}]
+(defn drawSolidCircle [& {:keys [x y radius fill]}]
+  (set! (.-fillStyle context) fill)
   (.beginPath context)
   (.arc context x y radius 0 doublePi)
   (.closePath context)
   (.fill context))
 
-(defn drawEmptyCircle [& {:keys [x y radius]}]
+(defn drawEmptyCircle [& {:keys [x y radius fill]}]
+  (set! (.-fillStyle context) fill)
   (.beginPath context)
   (.arc context x y radius 0 doublePi)
   (.closePath context)
   (.stroke context))
 
-(drawSolidCircle :x (/ width 2) :y (/ height 2) :radius 25)
-(drawEmptyCircle :x (/ width 2) :y (/ height 2) :radius 250)
-(drawBigHand :x (/ width 2) :y (/ height 2))
+(defn drawHand [pos]
+  (.beginPath context)
+  (.moveTo context 0 0)
+  (.rotate context pos)
+  (.lineTo context 0 (- 200))
+  (.stroke context)
+  (.rotate context (- pos)))
+
+(defn drawClock []
+  (def seconds (.getSeconds (js/Date.)))
+  (drawSolidCircle :x 0 :y 0 :radius 250 :fill "#fff")
+  (drawSolidCircle :x 0 :y 0 :radius 25 :fill "#00f")
+  (drawEmptyCircle :x 0 :y 0 :radius 250 :fill "#00f")
+  (drawHand (* seconds (/ pi 30))))
+
+(.translate context (/ width 2) (/ height 2))
+(js/setInterval drawClock 1000)
